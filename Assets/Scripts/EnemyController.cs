889d0423cdd8ec1b13a23fddc0a8b31f41776ba5
Bruno Silva <brunosilva_95@hotmail.com>
2020-09-02 16:08:50 +0100

    Add controller support

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[SerializeField] Rigidbody2D Rigidbody2D;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
+		StartCoroutine( Patrol() );
+	}
+
+	IEnumerator Patrol()
+	{
+		while ( true )
+		{
+			Rigidbody2D.velocity = Vector2.right * 2;
+
+			yield return new WaitForSeconds( 3 );
+
+			Rigidbody2D.velocity = Vector2.left * 2;
+			
+			yield return new WaitForSeconds( 3 );
+		}
+	}
 }
