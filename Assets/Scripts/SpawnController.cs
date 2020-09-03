436d1ca8dee3546520a675b09ac40d86ff9771f2
Bruno Silva <brunosilva_95@hotmail.com>
2020-09-03 12:30:02 +0100

    Make camera follow all the players

---
+++ Assets/Scripts/SpawnController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SpawnController : MonoBehaviour
+{
+    [SerializeField] CinemachineTargetGroup TargetGroup;
+
+    void OnPlayerJoined(PlayerInput input)
+    {
+        Debug.Log("joined");
+        TargetGroup.AddMember(input.gameObject.transform, 1,0 );
+    }
+}
