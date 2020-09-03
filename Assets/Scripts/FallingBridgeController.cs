5ec95eec8e97368cf463a98a497fdd6a0790f1af
Bruno Silva <brunosilva_95@hotmail.com>
2020-09-03 18:33:28 +0100

    Add bridge with trigger

---
+++ Assets/Scripts/FallingBridgeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingBridgeController : MonoBehaviour
+{
+	[SerializeField] Rigidbody2D Rigidbody2D;
+
+	bool IsLowered;
+
+	public void LowerBridge()
+	{
+		if ( !IsLowered )
+		{
+			Rigidbody2D.AddForceAtPosition(Vector2.left*150, transform.up);
+			IsLowered = true;
+		}
+	}
+}
---
+++ Assets/Scripts/TriggerController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerController : MonoBehaviour
+{
+    [SerializeField] FallingBridgeController target;
+    void OnTriggerEnter2D( Collider2D other )
+    {
+        if ( other.tag == "Player" )
+        {
+            target.LowerBridge();
+        }
+    }
+}
