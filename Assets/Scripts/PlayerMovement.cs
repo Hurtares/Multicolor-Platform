a9c6afda756ef2a94b626dfef62a48f45d31b03a
Bruno Silva <brunosilva_95@hotmail.com>
2020-09-03 16:52:41 +0100

    Format PlayerMovementScript

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,100 +8,87 @@ using Random = UnityEngine.Random;
 
 public class PlayerMovement : MonoBehaviour
 {
-    PlayerInputActions inputActions;
-    [SerializeField] Rigidbody2D Rigidbody2D;
+	[SerializeField] Rigidbody2D Rigidbody2D;
 
-    Vector2 direction;
-    bool IsMoving;
-    string currentColor= "Blue";
-    const float Speed = 10;
-    const float JumpStrength = 500;
+	Vector2 direction;
+	bool IsMoving;
+	string currentColor = "Blue";
+	const float Speed = 10;
+	const float JumpStrength = 500;
 
-    List<string> colors = new List<string>()
-    {
-        "Red", "Green", "Blue"
-        
-    };
+	List<string> colors = new List<string>()
+	{
+		"Red", "Green", "Blue"
+	};
 
-    void Awake()
-    {
-        inputActions = new PlayerInputActions();
-    }
+	void OnPreviousColor()
+	{ 
+		int index = colors.IndexOf( currentColor );
+		SetColor( colors[( index + 2 ) % 3] );
+	}
 
-    void OnEnable()
-    {
-        inputActions.Enable();
-    }
+	void OnNextColor()
+	{
+		int index = colors.IndexOf( currentColor );
+		SetColor( colors[( index + 1 ) % 3] );
+	}
 
-    void OnDisable()
-    {
-        inputActions.Disable();
-    }
+	void OnJump()
+	{
+		Rigidbody2D.AddForce( Vector2.up * JumpStrength );
+	}
 
-    void OnPreviousColor()
-    {
-        int index = colors.IndexOf( currentColor );
-        SetColor(colors[ (index + 2) % 3]);
-        Debug.Log("hhsdh");
-    }
-    
-    void OnNextColor()
-    {
-        int index = colors.IndexOf( currentColor );
-        SetColor(colors[(index + 1) % 3]);
-        Debug.Log("hhsdh2");
-    }
+	void OnMovement( InputValue value )
+	{
+		direction = value.Get<Vector2>();
+		IsMoving = Math.Abs( direction.x ) > .1f;
+		StartCoroutine( SetVelocity() );
+	}
 
-    void OnJump( )
-    {
-        Rigidbody2D.AddForce(Vector2.up * JumpStrength);
-    }
+	void SetColor( string color )
+	{
+		gameObject.layer = LayerMask.NameToLayer( color );
 
-    void OnMovement(InputValue value )
-    {
-        direction = value.Get<Vector2>();
-        IsMoving = Math.Abs( direction.x ) > .1f;
-        StartCoroutine( SetVelocity() );
-    }
-    
-    void SetColor( string color )
-    {
-        gameObject.layer = LayerMask.NameToLayer(color);
-        
-        if ( color == "Blue" )
-        {
-            GetComponent<SpriteRenderer>().color = Color.blue;
-            currentColor = color;
-            return;
-        }else if ( color == "Red"  )
-        {
-            GetComponent<SpriteRenderer>().color = Color.red;
-            currentColor = color;
-            return;
-        }
-        GetComponent<SpriteRenderer>().color = Color.green;
-        currentColor = color;
-    }
+		if ( color == "Blue" )
+		{
+			GetComponent<SpriteRenderer>().color = Color.blue;
+			currentColor = color;
 
-    IEnumerator SetVelocity()
-    {
-        while ( true )
-        {
-            Rigidbody2D.velocity = new Vector2(direction.x * Speed  , Rigidbody2D.velocity.y);
-            yield return new WaitForEndOfFrame();
+			return;
+		}
+		else if ( color == "Red" )
+		{
+			GetComponent<SpriteRenderer>().color = Color.red;
+			currentColor = color;
 
-            if ( !IsMoving )
-            {
-                yield break;
-            }
-        }
-    }
+			return;
+		}
 
-    void OnCollisionEnter2D( Collision2D other )
-    {
-        if ( other.gameObject.CompareTag( "Enemy" ) )
-        {
-            transform.position = new Vector3(-10,0,0);
-        }
-    }
+		GetComponent<SpriteRenderer>().color = Color.green;
+		currentColor = color;
+	}
+
+	IEnumerator SetVelocity()
+	{
+		while ( true )
+		{
+			Rigidbody2D.velocity = new Vector2( direction.x * Speed, Rigidbody2D.velocity.y );
+
+			yield return new WaitForEndOfFrame();
+
+			if ( !IsMoving )
+			{
+				yield break;
+			}
+		}
+	}
+
+	void OnCollisionEnter2D( Collision2D other )
+	{
+		if ( other.gameObject.CompareTag( "Enemy" ) )
+		{
+			//Todo: remove hard coded spawn
+			transform.position = new Vector3( -10, 0, 0 );
+		}
+	}
 }
